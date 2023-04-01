903024a0eb7c81c8c84d2c479e99f829db65b6ea
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-04-01 16:18:55 -0300

    feat(4): Install and configure drf-spectacular
    This commit installs and configures drf-spectacular to generate API documentation.
    The following endpoints have been added:
    - /api/schema/
    - /api/schema/swagger-ui/
    - /api/schema/redoc/

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',              # swagger docs
     'minhashoras_apps.accounts.apps.AccountsConfig',
 ]
 
@@ -144,3 +145,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# DRF settings
+# ------------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'MinhasHoras API',
+    'DESCRIPTION': 'Time tracking application',
+    'VERSION': '0.1.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API base url
     path("api/", include("config.api_router")),
+
+    # drf-spectacular
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
