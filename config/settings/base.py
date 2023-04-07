266b980e782a112d00fc57c83195d65fd25efa0c
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-04-07 09:38:59 -0300

    feat(9): Configure Logging

--- config/settings/base.py
+++ config/settings/base.py
@@ -163,3 +163,26 @@ SPECTACULAR_SETTINGS = {
 # Auditlog settings
 # ------------------------------------------------------------------------------
 AUDITLOG_INCLUDE_ALL_MODELS = True
+
+
+# LOGGING
+# ------------------------------------------------------------------------------
+LOG_LEVEL = env.str('DJANGO_LOG_LEVEL', default='INFO')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+            '%(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': LOG_LEVEL,
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        }
+    },
+    'root': {'level': LOG_LEVEL, 'handlers': ['console']},
+}
