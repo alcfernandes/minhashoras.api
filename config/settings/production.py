fd8b9b7ab058bb72f60db2c1666acaba00f71d28
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-04-28 13:43:34 -0300

    Config deploy on Vercel

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,12 @@ from .base import *  # noqa
 # GENERAL
 ALLOWED_HOSTS = env.list(
     'DJANGO_ALLOWED_HOSTS',
-    default=['minhashoras-api.fly.dev', 'localhost', '127.0.0.1'],
+    default=[
+        'minhashoras-api.fly.dev',
+        'localhost',
+        '127.0.0.1',
+        '.vercel.app',
+    ],
 )
 CSRF_TRUSTED_ORIGINS = env.list(
     'DJANGO_CSRF_TRUSTED_ORIGINS', default=['https://minhashoras-api.fly.dev']
