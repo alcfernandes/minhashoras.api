6ce36020f647c5a8db1e80a290b7184fa4f2011c
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-01 14:59:10 -0300

    Add python-decouple and refact settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,9 +1,1 @@
 from .base import *  # noqa
-
-# GENERAL
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
-
-# Cross-origin requests config
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:5173',
-]
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,19 +1,1 @@
 from .base import *  # noqa
-
-# GENERAL
-ALLOWED_HOSTS = env.list(
-    'DJANGO_ALLOWED_HOSTS',
-    default=[
-        'minhashoras-api.fly.dev',
-        'localhost',
-        '127.0.0.1',
-    ],
-)
-CSRF_TRUSTED_ORIGINS = env.list(
-    'DJANGO_CSRF_TRUSTED_ORIGINS', default=['https://minhashoras-api.fly.dev']
-)
-
-# Cross-origin requests config
-CORS_ORIGIN_WHITELIST = [
-    'https://minhashoras-app-weld.vercel.app',
-]
