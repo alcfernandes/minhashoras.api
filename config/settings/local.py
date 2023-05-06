bcfdd5f263e64cf77504ed904569f5d9f7878f18
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-06 12:02:57 -0300

    Add django-extensions to local requirements

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,3 +5,9 @@ from .base import *  # noqa
 INSTALLED_APPS += ['debug_toolbar']  # noqa F405
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
+
+
+# django-extensions
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ['django_extensions']  # noqa F405
+SHELL_PLUS = 'ipython'
