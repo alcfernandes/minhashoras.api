4a2ca5048634e9a8b70f9897570f87530114a369
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-06 19:24:09 -0300

    build: Use python-decouple to load DJANGO_SETTINGS_MODULE in manage.py
    This commit updates the manage.py file to use python-decouple for
    loading the DJANGO_SETTINGS_MODULE environment variable from the .env
    file. It falls back to the default 'config.settings.local' if the
    variable is not set in the .env file.
    
    It's fix a build erro to fly.io.

--- manage.py
+++ manage.py
@@ -3,17 +3,23 @@
 import os
 import sys
 
+from decouple import config
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        config('DJANGO_SETTINGS_MODULE', default='config.settings.local'),
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            'available on your PYTHONPATH environment variable? Did you '
+            'forget to activate a virtual environment?'
         ) from exc
     execute_from_command_line(sys.argv)
 
