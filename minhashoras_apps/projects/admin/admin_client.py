1935be061a6d6ab52390d6ac692f1c40ad9a629c
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-13 11:52:40 -0300

    feat(projects): Add account info on client and project admin
    Add account on list_display and implemente some search fields.

--- minhashoras_apps/projects/admin/admin_client.py
+++ minhashoras_apps/projects/admin/admin_client.py
@@ -2,4 +2,12 @@ from minhashoras_apps.core.admin import BaseAdmin
 
 
 class ClientAdmin(BaseAdmin):
-    list_display = ['id', 'name', 'email', 'is_active']
+    list_display = ['id', 'name', 'email', 'account', 'is_active']
+    raw_id_fields = [
+        'account',
+    ]
+    search_fields = [
+        'name',
+        'account__owner__email',
+        'account__id',
+    ]
--- minhashoras_apps/projects/admin/admin_project.py
+++ minhashoras_apps/projects/admin/admin_project.py
@@ -2,4 +2,12 @@ from minhashoras_apps.core.admin import BaseAdmin
 
 
 class ProjectAdmin(BaseAdmin):
-    list_display = ['id', 'name', 'is_active']
+    list_display = ['id', 'name', 'client', 'account', 'is_active']
+    raw_id_fields = ['account', 'client']
+    search_fields = [
+        'name',
+        'client__name',
+        'account__owner__email',
+        'account__id',
+        'client__id',
+    ]
