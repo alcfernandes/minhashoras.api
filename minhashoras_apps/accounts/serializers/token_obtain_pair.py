93ca13d6ef6f72abaff3aeec6f8736da2b20b966
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-15 17:47:48 -0300

    feat(auth): Ensure just users with accounts obtain a token
    If user does not belong to any account raise an
    AuthenticationFailed exception.

--- minhashoras_apps/accounts/serializers/token_obtain_pair.py
+++ minhashoras_apps/accounts/serializers/token_obtain_pair.py
@@ -1,11 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import exceptions
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
+        if not user.account:
+            raise exceptions.AuthenticationFailed(
+                _('The user does not belong to any account.')
+            )
+
         token = super().get_token(user)
 
-        token['account_uuid'] = f'{user.account.uuid}' if user.account else ''
+        token['account_uuid'] = str(user.account.uuid)
 
         return token
