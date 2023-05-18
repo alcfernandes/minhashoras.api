38b30324f2daf2aa84d5c217dd32ebbd183235b2
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-05-18 17:04:20 -0300

    refactor(accounts.admin): Change imports order

--- minhashoras_apps/accounts/admin/__init__.py
+++ minhashoras_apps/accounts/admin/__init__.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Import the models
 from ..models import Account, User
-from .admin_account import AccountAdmin
 
 # Import the custom admin classes
+from .admin_account import AccountAdmin
 from .admin_user import UserAdmin
 
 # Register the models with the custom admin classes
