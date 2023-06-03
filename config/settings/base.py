0780187e4f6b559a8a7ff644a53d5219e3353e86
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-06-03 18:57:28 -0300

    feat(API): Implement pagination for all views

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,6 +186,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'minhashoras_apps.core.custom_paginations.CustomPageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 # SIMPLE JWT SETTINGS
